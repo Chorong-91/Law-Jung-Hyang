6be4130bf06503b26c7fc3e48ef945b75fe490fb
Chorong-91 <89567894+Chorong-91@users.noreply.github.com>
2021-08-26 17:01:36 +0900

    Add files via upload

---
+++ sub/team/foreign/kim_sc.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php include $_SERVER['DOCUMENT_ROOT']."/include/head.php"; ?>
+</head>
+<body>
+<div id="wrap" class="sub_wrap">
+	<div class="wrapper">
+
+		<!-- container -->
+		<div id="container">
+
+			<!-- header -->
+			<?php include $_SERVER['DOCUMENT_ROOT']."/include/header.php"; ?>
+			<!-- //header -->
+
+			<!-- content -->
+			<div id="contents">
+				<div class="subVisual" style="background:url(/images/sub/team/team-visual.jpg) center no-repeat;">
+					<?php include $_SERVER['DOCUMENT_ROOT']."/sub/team/subTab.php"; ?>
+				</div>
+				<!-- ===================== 내용 ===================== -->
+				<div class="contentsArea">
+					<?php include $_SERVER['DOCUMENT_ROOT']."/sub/team/foreign/contents/".$path.".php"; ?>
+				</div>
+				<!-- ===================== //내용 ===================== -->
+			</div>
+			<!-- //content -->
+
+			<!-- footer -->
+			<?php include $_SERVER['DOCUMENT_ROOT']."/include/footer.php"; ?>
+			<!-- //footer -->
+
+		</div>
+		<!-- //container -->
+	</div>
+</div>
+</body>
+</html>
